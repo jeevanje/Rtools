8960af73faa1d697f16bb4e77b57e0b750db97c4
nadir_jeevanjee <njeevanjee@lbl.gov>
2016-09-27 12:58:10 -0700

    Added gcm_tools.R, modified nc_tools.R

--- nc_tools.R
+++ nc_tools.R
@@ -12,3 +12,11 @@ get_profile = function(nc,nt_avg,varname){
 		var = apply(var2D,1,mean)
 		return(var)      
 		}
+
+get_avg     = function(nc,nt_avg,varname){
+                time = get.var.ncdf(nc,"time")
+                nt = length(time)
+                var1D = get.var.ncdf(nc,start=c(nt-nt_avg),varname)
+                var = mean(var1D)
+                return(var)
+                }                            
