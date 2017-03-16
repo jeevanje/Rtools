1c9759696b153c0e5a33f5dbfe92a41ca1a66d82
Nadir Jeevanjee <Nadir.Jeevanjee@ldt-1467466.gfdl.noaa.gov>
2017-03-16 09:46:21 -0400

    Added get_slice, get_profile, get_streamfunction to nc_tools.R Modified buoyancy_func in thermo_tools to handle 3 or 4d variables. Added add_buoyancy.R to add B field to DAM snapshots.

--- nc_tools.R
+++ nc_tools.R
@@ -5,6 +5,8 @@
 # files      #
 #============#
 
+source("~/Dropbox/Rtools/calculus_tools.R")
+
 get_profile = function(nc,nt_avg,varname){
 	        time = get.var.ncdf(nc,"time")
 		nt = length(time)
@@ -12,3 +14,29 @@ get_profile = function(nc,nt_avg,varname){
 		var = apply(var2D,1,mean)
 		return(var)      
 		}
+
+get_slice = function(nc,nt_avg,varname){
+	        time = get.var.ncdf(nc,"time")
+		nt = length(time)
+	        var3D = get.var.ncdf(nc,start=c(1,1,nt-nt_avg),varname)
+		var = apply(var3D,1:2,mean)
+		return(var)      
+		}
+
+get_streamfunction = function(nc,nt_avg){
+	        z    = get.var.ncdf(nc,"z")		   
+		nz   = length(z)
+		zint = zinterp(z)
+		dz   = c(diff(zint),z[nz]-z[nz-1])
+		rho    = get_slice(nc,nt_avg,"rho") 
+		v    = get_slice(nc,nt_avg,"v") 
+		w    = get_slice(nc,nt_avg,"w") 
+		ny   = dim(v)[1]
+		psi  = array(dim=c(ny,nz))
+		psi[ ,1]<-(rho*v)[ ,1]*dz[1]
+		for (j in 2:nz){
+			psi[,j]<- ((rho*v)[ ,1:j])%*%dz[1:j]
+			}
+		return(psi)
+		}
+
