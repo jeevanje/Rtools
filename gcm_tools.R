f445dc0d4452cb3411e1ee2400981c6e776d991c
nadir_jeevanjee <njeevanjee@lbl.gov>
2017-07-05 12:12:32 -0700

    Added 2 or 3d capability to function global_mean

--- gcm_tools.R
+++ gcm_tools.R
@@ -4,16 +4,43 @@
 #===============#
 
 global_mean = function(lon,lat,field){
-		# assumes lat in degrees!
-		R    = 6.36e6  # m
-		nlon = length(lon)
-		nlat = length(lat)
-		dlon = lon[2]-lon[1]
-		dlat = lat[2]-lat[1]
-		dlonvec = rep.int(dlon,nlon)
-		dlatvec= rep.int(dlat,nlat)
-		area = (pi/180)^2*R^2*dlonvec%o%(cos(pi/180*lat)*dlatvec)
-		mean = sum(field*area)/sum(area)
+		# assumes lat/lon in degrees!
+		lat_rad  = pi/180*lat
+		lon_rad  = pi/180*lon
+		nlon     = length(lon)	
+		nlat     = length(lat)
+		dlon_rad = lon_rad[2]-lon_rad[1]
+		dlat_rad = lat_rad[2]-lat_rad[1]
+		dlonvec  = rep.int(dlon_rad,nlon)
+		dlatvec  = rep.int(dlat_rad,nlat)
+		if (length(dim(field)) ==3 ){
+		   n3d = dim(field)[3]
+		   d3dvec = rep.int(1,n3d)
+		   domega = dlonvec%o%(cos(lat_rad)*dlatvec)%o%d3dvec
+		   } else if ( length(dim(field)) ==2 ){	
+		   domega = dlonvec%o%(cos(lat_rad)*dlatvec) 
+		   } else {
+		   print("dim(field) neq 2 or 3. Stopping")
+		   return()
+		   }	
+		domega[is.na(field)] = NA
+		mean = sum(field*domega,na.rm=TRUE)/sum(domega,na.rm=TRUE)
 		mean
 		}
+
+plot_map <- function(lon,lat,field,main,zlim=range(field),
+			do_mean = TRUE, units = NULL,cex = 1.15){
+            image.plot(lon,lat,field,zlim=zlim,
+                        xlab = "Longitude (deg)",
+			ylab = "Latitude (deg)", 
+                        main = main,
+			cex.lab = cex,
+			cex.main = cex,
+			cex.axis = cex)
+            map("world2",add=TRUE,interior=FALSE)
+            title("")
+            mean = global_mean(lon,lat,field)
+            mtext(bquote("Global mean = "~.(round(mean,1))~.(units)),at=360)
+            }
+
 
